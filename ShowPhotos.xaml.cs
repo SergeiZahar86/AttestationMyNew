0f6203f17a88f5089582c1f4b60da61756b2d594
Сергей Захаров <sergei86zahar@mail.ru>
2020-10-21 11:16:59 +0700

    1

--- ShowPhotos.xaml.cs
+++ ShowPhotos.xaml.cs
@@ -1,33 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Attestation
 {
-    /// <summary>
-    /// Логика взаимодействия для ShowPhotos.xaml
-    /// </summary>
     public partial class ShowPhotos : Window
     {
         private Global global;
-        public int idx;
-        public List<RowTab> DATA;
-
-
         public ShowPhotos()
         {
             InitializeComponent();
-            /*
-            global = Global.getInstance();
-            idx = global.Idx;
-            DATA = global.DATA;
-            */
         }
-
-
         private void close_ShowPhotos(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
     }
-    
 }
