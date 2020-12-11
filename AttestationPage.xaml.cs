8d80e86e2706088c149d2d9c355ed06236e43137
Сергей Захаров <sergei86zahar@mail.ru>
2020-12-11 10:32:30 +0700

    1

--- AttestationPage.xaml.cs
+++ AttestationPage.xaml.cs
@@ -41,7 +41,7 @@ namespace Attestation
             // Таймер ///////////////////////////////////////
             dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(OnTimedEvent);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
 
             /////////////////////////////////////////////////
             InitializeComponent();
